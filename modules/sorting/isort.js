7c29eb62d935c125f8dc601cbab8bc912cec9186
curist <curist.cyc@gmail.com>
2012-10-04 09:45:35 +0800

    insertion sort added

---
+++ modules/sorting/isort.js
@@ -0,0 +1,24 @@
+module.exports.sort = function insertion_sort(array){
+  var i
+    , j
+    , min
+    , min_index
+    , tmp
+    , len = array.length
+    , working_arr = array.slice();
+
+  for(i = 0; i < len-1; i++) {
+    min = Infinity;
+    for(j = i; j < len; j++) {
+      if(working_arr[j] < min) {
+        min = working_arr[j];
+        min_index = j;
+      }
+    }
+    tmp = working_arr[i];
+    working_arr[i] = working_arr[min_index];
+    working_arr[min_index] = tmp;
+  }
+  return working_arr;
+};
+
